277179e60ea48de09171b7f940253614ba145e34
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-06-01 06:16:16 -0300

    hashmap

--- src/br/edu/utfpr/cm/algoritmo/ArvoreGeradoraRotulosMinimos.java
+++ src/br/edu/utfpr/cm/algoritmo/ArvoreGeradoraRotulosMinimos.java
@@ -5,8 +5,13 @@
  */
 package br.edu.utfpr.cm.algoritmo;
 
+import br.edu.utfpr.cm.algoritmo.entidades.VerticeRotulosMinimos;
+import br.edu.utfpr.cm.grafo.ArestaPonderada;
+import br.edu.utfpr.cm.grafo.Grafo;
 import br.edu.utfpr.cm.grafo.Vertice;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,24 +25,38 @@ public class ArvoreGeradoraRotulosMinimos implements Algoritmo {
     /**
      * OPEN is the storage place for all generated but unexpanded nodes.
      */
-    private List<Vertice> open;
+    private List<VerticeRotulosMinimos> open;
 
     /**
      * CLOSED is the storage place for the expanded nodes.
      */
-    private List<Vertice> closed;
+    private List<VerticeRotulosMinimos> closed;
 
-    private Vertice root;
+    private Grafo grafo;
 
-    public ArvoreGeradoraRotulosMinimos() {
+    private HashMap<String, Integer> unusedLabelsHasEdgesCovered;
+    private List<String> selectedLabels;
+
+    private VerticeRotulosMinimos root;
+
+    public ArvoreGeradoraRotulosMinimos(Grafo grafo) {
+        this.grafo = grafo;
         open = new ArrayList<>();
         closed = new ArrayList<>();
+        unusedLabelsHasEdgesCovered = new HashMap<>();
+        selectedLabels = new ArrayList<>();
+
+        Iterator<VerticeRotulosMinimos> vertices = this.grafo.getVertices();
+        while (vertices.hasNext()) {
+            VerticeRotulosMinimos next = vertices.next();
+            unusedLabelsHasEdgesCovered.put(next.getId(), getNumberOfEdgesCovered(next.getId()));
+        }
 
         //1. Put the root node r on OPEN.
         open.add(root);
     }
 
-    public Vertice heuristica() {
+    private VerticeRotulosMinimos heuristica() {
         //If there are more than one node with the same minimum f value, the latest generated node will be selected.
         throw new UnsupportedOperationException();
     }
@@ -46,7 +65,7 @@ public class ArvoreGeradoraRotulosMinimos implements Algoritmo {
     //Output: A spanning tree with minimum number of tree edge labels.
     @Override
     public void executar() {
-        Vertice minimumF;
+        VerticeRotulosMinimos minimumF;
         try {
             while (true) {
                 //2. If OPEN is empty, exit with failure.
@@ -55,7 +74,7 @@ public class ArvoreGeradoraRotulosMinimos implements Algoritmo {
                     throw new Exception("Falha");
                 }
                 //3. Remove from OPEN and place on CLOSED a node n for which f is minimum.
-                minimumF = heuristica();
+                minimumF = getMinimumF();
                 open.remove(minimumF);
                 closed.add(minimumF);
                 //4. If n is a goal node (a spanning subgraph is formed), goto Step 8.
@@ -64,12 +83,13 @@ public class ArvoreGeradoraRotulosMinimos implements Algoritmo {
                 } else {
                     //5. Otherwise expand n.
                     //If there are k unselected labels, then n has k children, one for each unselected label.
-                    for (Vertice son : open) {
+                    for (VerticeRotulosMinimos son : open) {
                         //6. For each child son of n
                         //If n1 is not already on OPEN or CLOSE
                         if (!open.contains(son) && !closed.contains(son)) {
                             //calculate f(son) = g(son) + h(son)
                             //where g(son) = g(n) + 1 and g(r) = 0.
+                            son.setF(0);
                             //Put n1 into OPEN.
                             open.add(son);
                         }
@@ -82,7 +102,33 @@ public class ArvoreGeradoraRotulosMinimos implements Algoritmo {
         }
         throw new UnsupportedOperationException();
     }
-    
-    
+
+    private VerticeRotulosMinimos getMinimumF() {
+        if (open == null) {
+            return null;
+        }
+        if (open.size() == 0) {
+            return null;
+        }
+        VerticeRotulosMinimos minimumF = open.get(0);
+        for (VerticeRotulosMinimos verticeRotulosMinimos : open) {
+            if (verticeRotulosMinimos.getF() < minimumF.getF()) {
+                minimumF = verticeRotulosMinimos;
+            }
+        }
+        return minimumF;
+    }
+
+    private Integer getNumberOfEdgesCovered(String id) {
+        int numberOfEdgesCovered = 0;
+        Iterator<ArestaPonderada> arestas = grafo.getArestas();
+        while (arestas.hasNext()) {
+            ArestaPonderada next = arestas.next();
+            if (id.equals(next.getPeso())) {
+                numberOfEdgesCovered++;
+            }
+        }
+        return numberOfEdgesCovered * 2;
+    }
 
 }
---
+++ src/br/edu/utfpr/cm/algoritmo/entidades/VerticeRotulosMinimos.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.algoritmo.entidades;
+
+import br.edu.utfpr.cm.grafo.Vertice;
+
+/**
+ *
+ * @author romulo
+ */
+public class VerticeRotulosMinimos extends Vertice {
+
+    private int f;
+
+    public int getF() {
+        return f;
+    }
+
+    public void setF(int f) {
+        this.f = f;
+    }
+
+}
