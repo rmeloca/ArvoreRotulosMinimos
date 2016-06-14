8c1c9af69f186427ddb78da497b67b5435b12f52
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-06-14 00:54:59 -0300

    remove fuse

--- test/br/edu/utfpr/cm/algoritmo/AestrelaTest.java
+++ test/br/edu/utfpr/cm/algoritmo/AestrelaTest.java
@@ -8,7 +8,9 @@ package br.edu.utfpr.cm.algoritmo;
 import br.edu.utfpr.cm.factory.GrafoFactory;
 import br.edu.utfpr.cm.factory.Orientacao;
 import br.edu.utfpr.cm.factory.Representacao;
+import br.edu.utfpr.cm.grafo.ArestaPonderada;
 import br.edu.utfpr.cm.grafo.Grafo;
+import br.edu.utfpr.cm.grafo.Vertice;
 import org.junit.Test;
 
 /**
@@ -21,7 +23,19 @@ public class AestrelaTest {
 
     @Test
     public void testarAEstrela() {
-        Grafo grafo = GrafoFactory.constroiGrafo(Representacao.LISTA_ADJACENCIA, Orientacao.NAO_DIRIGIDO);
+        Grafo grafo = GrafoFactory.constroiGrafo(Representacao.MATRIZ_ADJACENCIA, Orientacao.NAO_DIRIGIDO);
+        
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("1"), new Vertice("2"), 1));
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("3"), new Vertice("2"), 2));
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("2"), 4));
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("2"), new Vertice("2"), 2));
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("2"), 7));
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("7"), 2));
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("2"), 9));
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("6"), 3));
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("6"), 2));
+        grafo.adicionaAresta(new ArestaPonderada(new Vertice("8"), new Vertice("6"), 7));
+        
         this.aestrela = new Aestrela(grafo);
         this.aestrela.execute();
     }
