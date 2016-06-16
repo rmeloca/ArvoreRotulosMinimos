29b54343ae8d3ab351931fd5ab92a18f2d3cdd19
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-06-15 23:43:53 -0300

    relatorio

--- test/br/edu/utfpr/cm/algoritmo/AestrelaTest.java
+++ test/br/edu/utfpr/cm/algoritmo/AestrelaTest.java
@@ -11,6 +11,11 @@ import br.edu.utfpr.cm.factory.Representacao;
 import br.edu.utfpr.cm.grafo.ArestaPonderada;
 import br.edu.utfpr.cm.grafo.Grafo;
 import br.edu.utfpr.cm.grafo.Vertice;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.Test;
 
 /**
@@ -23,21 +28,20 @@ public class AestrelaTest {
 
     @Test
     public void testarAEstrela() {
-        Grafo grafo = GrafoFactory.constroiGrafo(Representacao.MATRIZ_ADJACENCIA, Orientacao.NAO_DIRIGIDO);
-        
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("1"), new Vertice("2"), 1));
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("3"), new Vertice("2"), 2));
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("2"), 4));
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("2"), new Vertice("2"), 2));
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("2"), 7));
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("7"), 2));
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("2"), 9));
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("6"), 3));
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("5"), new Vertice("6"), 2));
-        grafo.adicionaAresta(new ArestaPonderada(new Vertice("8"), new Vertice("6"), 7));
-        
-        this.aestrela = new Aestrela(grafo);
-        this.aestrela.execute();
+
+        InputStream inputStream;
+        List<Grafo> lista;
+        try {
+            inputStream = (getClass().getResourceAsStream("../instancias/group_1/HDGraph20_20.txt"));
+            lista = GrafoFactory.lerGrafos(Representacao.MATRIZ_ADJACENCIA, Orientacao.DIRIGIDO, inputStream);
+            Grafo grafo = lista.get(0);
+
+            this.aestrela = new Aestrela(grafo);
+            this.aestrela.execute();
+
+        } catch (IOException ex) {
+            Logger.getLogger(AestrelaTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
 }
