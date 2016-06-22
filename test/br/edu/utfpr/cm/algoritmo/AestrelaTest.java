1e69d7b7d0bfc946dacd7c3e48d8d65ab8067cb1
marcocmm <marco.cmm@gmail.com>
2016-06-22 02:51:57 -0300

    Teste

--- test/br/edu/utfpr/cm/algoritmo/AestrelaTest.java
+++ test/br/edu/utfpr/cm/algoritmo/AestrelaTest.java
@@ -29,7 +29,9 @@ public class AestrelaTest {
 
     @Test
     public void testarAEstrela() {
-
+        long tempoInicial;
+        long tempoTotal;
+        short qtdGrafosExec;
         InputStream arquivoStream;
         List<Grafo<VerticeBuscaProfundidade, Aresta<VerticeBuscaProfundidade, VerticeBuscaProfundidade>>> lista;
         List<Aresta> resultado;
@@ -38,30 +40,49 @@ public class AestrelaTest {
         try {
 //            arquivosTeste.add("../instancias/group_1/LDGraph20_20.txt");
 //            arquivosTeste.add("../instancias/group_1/MDGraph20_20.txt");
-            arquivosTeste.add("../instancias/group_1/HDGraph20_20.txt");
-//            arquivosTeste.add("../instancias/group_1/LDGraph30_30.txt");
-//            arquivosTeste.add("../instancias/group_1/MDGraph30_30.txt");
+//            arquivosTeste.add("../instancias/group_1/HDGraph20_20.txt");
 //            arquivosTeste.add("../instancias/group_1/HDGraph30_30.txt");
-//            arquivosTeste.add("../instancias/group_1/LDGraph40_40.txt");
-//            arquivosTeste.add("../instancias/group_1/MDGraph40_40.txt");
+//            arquivosTeste.add("../instancias/group_1/MDGraph30_30.txt");
+//            arquivosTeste.add("../instancias/group_1/LDGraph30_30.txt");
 //            arquivosTeste.add("../instancias/group_1/HDGraph40_40.txt");
-//            arquivosTeste.add("../instancias/group_1/LDGraph50_50.txt");
-//            arquivosTeste.add("../instancias/group_1/MDGraph50_50.txt");
+//            arquivosTeste.add("../instancias/group_1/MDGraph40_40.txt");
+//            arquivosTeste.add("../instancias/group_1/LDGraph40_40.txt");
 //            arquivosTeste.add("../instancias/group_1/HDGraph50_50.txt");
+//            arquivosTeste.add("../instancias/group_1/MDGraph50_50.txt");
+//            arquivosTeste.add("../instancias/group_1/LDGraph50_50.txt");
+            arquivosTeste.add("../instancias/group_2_with_n=100/HDGraph100_100.txt");
+            arquivosTeste.add("../instancias/group_2_with_n=100/MDGraph100_100.txt");
+            arquivosTeste.add("../instancias/group_2_with_n=100/LDGraph100_100.txt");
+            
+//            arquivosTeste.add("../instancias/group_2_with_n=100/HDGraph100_125.txt");
+//            arquivosTeste.add("../instancias/group_2_with_n=100/MDGraph100_125.txt");
+//            arquivosTeste.add("../instancias/group_2_with_n=100/LDGraph100_125.txt");
+//            arquivosTeste.add("../instancias/group_2_with_n=100/HDGraph100_25.txt");
+//            arquivosTeste.add("../instancias/group_2_with_n=100/MDGraph100_25.txt");
+//            arquivosTeste.add("../instancias/group_2_with_n=100/LDGraph100_25.txt");
+//            arquivosTeste.add("../instancias/group_2_with_n=100/HDGraph100_50.txt");
+//            arquivosTeste.add("../instancias/group_2_with_n=100/MDGraph100_50.txt");
+//            arquivosTeste.add("../instancias/group_2_with_n=100/LDGraph100_50.txt");
+//            arquivosTeste.add("../instancias/group_2_with_n=500/HDGraph500_625.txt");
             for (String arquivoNome : arquivosTeste) {
                 System.out.println("@@@ " + arquivoNome + " @@@");
                 arquivoStream = (getClass().getResourceAsStream(arquivoNome));
                 lista = GrafoFactory.lerGrafos(Representacao.MATRIZ_ADJACENCIA, Orientacao.NAO_DIRIGIDO, arquivoStream);
-
+                tempoInicial = System.currentTimeMillis();
+                qtdGrafosExec = 0;
                 for (Grafo<VerticeBuscaProfundidade, Aresta<VerticeBuscaProfundidade, VerticeBuscaProfundidade>> grafo : lista) {
                     this.aestrela = new Aestrela(grafo);
                     this.aestrela.executar();
-                    resultado = this.aestrela.getSpanningTree();
-                    System.out.println("*** Grafo: " + grafo.getId() + " ***");
-                    for (Aresta aresta : resultado) {
-                        System.out.println(aresta.getVertice1() + " --" + aresta.getPeso() + "-- " + aresta.getVertice2());
-                    }
+                    qtdGrafosExec++;
+//                    resultado = this.aestrela.getSpanningTree();
+//                    System.out.println("*** Grafo: " + grafo.getId() + " ***");
+//                    for (Aresta aresta : resultado) {
+//                        System.out.println(aresta.getVertice1() + " --" + aresta.getPeso() + "-- " + aresta.getVertice2());
+//                    }
                 }
+                tempoTotal = System.currentTimeMillis() - tempoInicial;
+                System.out.println("Tempo de execução de " + qtdGrafosExec + " do grupo é de: " + tempoTotal);
+                System.out.println("Media de: " + (tempoTotal/qtdGrafosExec));
             }
         } catch (IOException ex) {
             Logger.getLogger(AestrelaTest.class.getName()).log(Level.SEVERE, null, ex);
